d2eb1d55281b6df165f9343457f3280d554a73b8
35832 <tongpeng@bonc.com.cn>
2018-09-19 17:32:03 +0800

    更新工具类

--- src/main/java/cn/com/thread/util/BusyFlagUtil.java
+++ src/main/java/cn/com/thread/util/BusyFlagUtil.java
@@ -4,11 +4,11 @@ public class BusyFlagUtil {
 
     protected Thread busyFalg = null;
     protected int busyCount = 0;
-    //等待锁被释放
-    public void getBusyFlag(){
-        while(tryGetBusyFlag() == false){
+    //等待同步锁被释放
+    public synchronized void  getBusyFlag(){
+        if(tryGetBusyFlag() == false){
             try {
-                Thread.sleep(100);
+                wait();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -28,9 +28,25 @@ public class BusyFlagUtil {
         }
         return false;
     }
+
+    public synchronized void freeBusyFlag(){
+        if(getBusyFlagOwner() == Thread.currentThread()){
+            busyCount--;
+            if(busyCount == 0){
+                busyFalg = null;
+                notify();
+            }
+        }
+    }
+
     //获取当前线程对象
     public synchronized Thread getBusyFlagOwner(){
         return busyFalg;
     }
 
+    public static void main(String args[]){
+        BusyFlagUtil busyFlagUtil = new BusyFlagUtil();
+        //System.out.println(busyFlagUtil.tryGetBusyFlag());
+        System.out.println(busyFlagUtil.getBusyFlagOwner());
+    }
 }
---
+++ src/main/java/cn/com/thread/util/ReturnMessageUtil.java
@@ -0,0 +1,24 @@
+package cn.com.thread.util;
+
+public class ReturnMessageUtil {
+
+    public static String MESSAGE_SUCCESS = "SUCCESS";
+
+    public static String MESSAGE_SUCCESS_CODE = "0000";
+
+    public static String MESSAGE_ERROR="ERROR";
+
+    public static String MESSAGE_ERROR_CODE="0001";
+
+    public static String MESSAGE_SYSTEM_ERROR="SYSTEM_ERROR";
+
+    public static String MESSAGE_SYSTEM_ERROR_CODE="0002";
+
+    public static String MESSAGE_PARAM_ERROR="PARAM_ERROR";
+
+    public static String MESSAGE_PARAM_ERROR_CODE="0003";
+
+    public static String MESSAGE_SOCKET_TIME_OUT = "SOCKET_TIME_OUT";
+
+    public static String MESSAGE_SOCKET_TIME_OUT_CODE = "0004";
+}
